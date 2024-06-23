98909a68f99202bb128a834a2347867c9da2a734
Mocha <126608230+MochaTheBear@users.noreply.github.com>
2024-06-22 20:20:58 -0400

    Final Commit

--- prob3.py
+++ prob3.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 d = timedelta(microseconds=-1)
-#print(d.days, d.seconds, d.microseconds)
+print(d.days, d.seconds, d.microseconds)
 
 correctTime = timedelta(days=100, hours=10, minutes=13)
-#print(correctTime.days, correctTime.seconds, correctTime.microseconds)
+print(correctTime.days, correctTime.seconds, correctTime.microseconds)
